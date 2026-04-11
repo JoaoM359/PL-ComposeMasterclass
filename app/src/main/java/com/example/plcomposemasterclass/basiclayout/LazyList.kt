74dc71db3b6e000071692bd2e4102090b54a3e36
JoaoM359 <359.joao@gmail.com>
2026-04-11 11:38:36 +0100

    Basic Layout - Lazy List

---
+++ app/src/main/java/com/example/plcomposemasterclass/basiclayout/LazyList.kt
@@ -0,0 +1,42 @@
+package com.example.plcomposemasterclass.basiclayout
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.plcomposemasterclass.ui.theme.PLComposeMasterclassTheme
+import kotlin.random.Random
+
+@Composable
+fun LazyListDemo(modifier: Modifier = Modifier) {
+    LazyRow(
+        modifier = modifier
+            .fillMaxSize(),
+    ) {
+        items(100) {
+            Box(
+                modifier = Modifier
+                    .size(100.dp)
+                    .background(Color(Random.nextInt()))
+            )
+        }
+
+
+    }
+}
+
+@Preview(
+    showBackground = true
+)
+@Composable
+private fun FlexLayoutDemoPreview() {
+    PLComposeMasterclassTheme {
+        LazyListDemo()
+    }
+}
