15da2e9c9fe28513360f307d9ce00fd88fd4b80a
JoaoM359 <359.joao@gmail.com>
2026-05-04 12:52:43 +0100

    Basic Modifiers - Spacings & Window Insets

--- app/src/main/java/com/example/plcomposemasterclass/MainActivity.kt
+++ app/src/main/java/com/example/plcomposemasterclass/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeGestures
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.plcomposemasterclass.basicmodifiers.SpacingModifiersDemo
 import com.example.plcomposemasterclass.statemanagement.numberguess.NumberGuessScreenRoot
 import com.example.plcomposemasterclass.ui.theme.PLComposeMasterclassTheme
 
@@ -20,10 +24,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PLComposeMasterclassTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    NumberGuessScreenRoot(
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    contentWindowInsets = WindowInsets.safeGestures
+                ) { innerPadding ->
+                    SpacingModifiersDemo(
                         modifier = Modifier
                             .padding(innerPadding)
+                            .consumeWindowInsets(innerPadding)
                     )
                 }
             }
---
+++ app/src/main/java/com/example/plcomposemasterclass/basicmodifiers/SpacingModifiers.kt
@@ -0,0 +1,62 @@
+package com.example.plcomposemasterclass.basicmodifiers
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imeNestedScroll
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.plcomposemasterclass.ui.theme.PLComposeMasterclassTheme
+
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+fun SpacingModifiersDemo(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .background(Color.Red)
+            .fillMaxSize(),
+    ) {
+
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .imeNestedScroll(),
+        ) {
+            items(100) {
+                Text(
+                    text = "Item $it",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                )
+            }
+        }
+
+        TextField(
+            value = "",
+            onValueChange = {},
+            modifier = Modifier.imePadding()
+        )
+    }
+}
+
+@Preview(
+    showBackground = true
+)
+@Composable
+private fun SpacingModifiersDemoPreview() {
+    PLComposeMasterclassTheme {
+        SpacingModifiersDemo()
+    }
+}
