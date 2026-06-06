7925fecb2087203f5880bc78066b4187f0c5bc14
JoaoM359 <359.joao@gmail.com>
2026-06-06 09:32:17 +0100

    Advanced Layouts, Measurements & Advanced Modifiers - Custom Lazy Mind Map Layout

--- app/src/main/java/com/example/plcomposemasterclass/MainActivity.kt
+++ app/src/main/java/com/example/plcomposemasterclass/MainActivity.kt
@@ -9,9 +9,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.plcomposemasterclass.advancedlayoutsmeasurementsandmodifiers.LazyScrollingDemo
+import androidx.compose.ui.unit.IntOffset
+import com.example.plcomposemasterclass.advancedlayoutsmeasurementsandmodifiers.LazyMindMap
+import com.example.plcomposemasterclass.advancedlayoutsmeasurementsandmodifiers.MindMapItem
 import com.example.plcomposemasterclass.ui.theme.PLComposeMasterclassTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,8 +30,49 @@ class MainActivity : ComponentActivity() {
                 Scaffold(
                     modifier = Modifier.fillMaxSize()
                 ) { innerPadding ->
-                    LazyScrollingDemo(
+                    val mindMapItems = remember {
+                        listOf(
+                            MindMapItem(
+                                title = "Hello World 1",
+                                percentageOffset = Offset(
+                                    x = 0f,
+                                    y = 0f
+                                )
+                            ),
+                            MindMapItem(
+                                title = "Hello World 2",
+                                percentageOffset = Offset(
+                                    x = 1f,
+                                    y = 0f
+                                )
+                            ),
+                            MindMapItem(
+                                title = "Hello World 3",
+                                percentageOffset = Offset(
+                                    x = 0.3f,
+                                    y = -0.5f
+                                )
+                            ),
+                            MindMapItem(
+                                title = "Hello World 4",
+                                percentageOffset = Offset(
+                                    x = -0.2f,
+                                    y = 1.5f
+                                )
+                            ),
+                        )
+                    }
+                    var mindMapOffset by remember {
+                        mutableStateOf(IntOffset.Zero)
+                    }
+                    LazyMindMap(
+                        items = mindMapItems,
+                        mindMapOffset = mindMapOffset,
+                        onDrag = { delta ->
+                            mindMapOffset += delta
+                        },
                         modifier = Modifier
+                            .fillMaxSize()
                             .padding(innerPadding)
                     )
                 }
