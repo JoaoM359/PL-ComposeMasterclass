1a66245a0e2508016de3db4fbd3e81c2fc83b04d
JoaoM359 <359.joao@gmail.com>
2026-06-20 15:09:38 +0100

    Side Effects & Effect Handlers - Why Effect Handlers Should Often Be Avoided

--- app/src/main/java/com/example/plcomposemasterclass/MainActivity.kt
+++ app/src/main/java/com/example/plcomposemasterclass/MainActivity.kt
@@ -6,18 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.plcomposemasterclass.sideeffectandeffecthandlers.DisposableEffectDemo
+import com.example.plcomposemasterclass.sideeffectandeffecthandlers.LaunchedEffectDemo
 import com.example.plcomposemasterclass.ui.theme.PLComposeMasterclassTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,25 +23,11 @@ class MainActivity : ComponentActivity() {
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
                 ) { innerPadding ->
-                    var toggle by remember {
-                        mutableStateOf(false)
-                    }
-
-                    if (!toggle) {
-                        DisposableEffectDemo()
-                    }
-
-                    Button(
-                        onClick = {
-                            toggle = !toggle
-                        },
+                    LaunchedEffectDemo(
                         modifier = Modifier
-                            .padding(innerPadding)
                             .fillMaxSize()
-                            .wrapContentSize()
-                    ){
-                        Text("Toglle")
-                    }
+                            .padding(innerPadding)
+                    )
                 }
             }
         }
