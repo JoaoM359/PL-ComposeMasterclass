69bf954cb1f4a094a61511ea6a202d861fae7a97
JoaoM359 <359.joao@gmail.com>
2026-08-06 14:10:12 +0100

    Performance Optimization - Reducing Overdraw

--- app/src/main/java/com/example/plcomposemasterclass/MainActivity.kt
+++ app/src/main/java/com/example/plcomposemasterclass/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
@@ -12,7 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.plcomposemasterclass.basicmodifiers.TriangleShape
 import com.example.plcomposemasterclass.compositionlocals.LocalShape
-import com.example.plcomposemasterclass.performanceoptimization.DeferredStateReadsDemo
+import com.example.plcomposemasterclass.performanceoptimization.ReducingOverdrawDemo
 import com.example.plcomposemasterclass.ui.theme.PLComposeMasterclassTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,10 +25,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             PLComposeMasterclassTheme {
                 CompositionLocalProvider(LocalShape provides TriangleShape) {
-                    DeferredStateReadsDemo(
-                        modifier = Modifier
-                            .fillMaxSize()
-                    )
+                    Scaffold(
+                        modifier = Modifier.fillMaxSize(),
+                    ) { innerPadding ->
+                        ReducingOverdrawDemo(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(innerPadding)
+                        )
+                    }
                 }
             }
         }
