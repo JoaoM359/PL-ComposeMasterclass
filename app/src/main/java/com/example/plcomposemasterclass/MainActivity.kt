a8fa91bb388979fe70366cf930b69f47b406cce1
JoaoM359 <359.joao@gmail.com>
2026-08-06 17:23:14 +0100

    Performance Optimization - Homework Assignment

--- app/src/main/java/com/example/plcomposemasterclass/MainActivity.kt
+++ app/src/main/java/com/example/plcomposemasterclass/MainActivity.kt
@@ -4,17 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.plcomposemasterclass.basicmodifiers.TriangleShape
-import com.example.plcomposemasterclass.compositionlocals.LocalShape
-import com.example.plcomposemasterclass.performanceoptimization.ReducingOverdrawDemo
+import com.example.plcomposemasterclass.performanceoptimization.homeworkassignment.ListItemScreenRoot
 import com.example.plcomposemasterclass.ui.theme.PLComposeMasterclassTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,17 +18,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PLComposeMasterclassTheme {
-                CompositionLocalProvider(LocalShape provides TriangleShape) {
-                    Scaffold(
-                        modifier = Modifier.fillMaxSize(),
-                    ) { innerPadding ->
-                        ReducingOverdrawDemo(
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .padding(innerPadding)
-                        )
-                    }
-                }
+                ListItemScreenRoot()
             }
         }
     }
